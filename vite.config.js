bb07fe78f04393494010f2f18509e86c0a37d485
mustafa egeh <Mr.mustafaegeh@gmail.com>
2025-11-25 16:41:13 +0200

    add:Core libraries in vite.cnfig & Optimize dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { icons } from "react-icons";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          framerMotion: ["framer-motion"],
+          icons: ["react-icons"],
+          utils: ["moment"],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react-icons"],
+  },
 });
